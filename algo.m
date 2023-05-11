565c959dcdefe23740c744e69d1648f0f1c8876a
nachofen <zorba037@gmail.com>
2023-05-11 11:55:12 -0700

    pint fix changed printf for fprintf

--- algo.m
+++ algo.m
@@ -1,6 +1,1 @@
-push 1
-push 1024
-push -0
-push -1
-push -1024
-push 123a
+pint
--- functions2.c
+++ functions2.c
@@ -75,7 +75,7 @@ void monty_pint(stack_t **stack, unsigned int line_number)
 {
 	if (!*stack)
 	{
-		printf("L%d:can\'t pint, stack empty", line_number);
+		fprintf(stderr, "L%d: can\'t pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
