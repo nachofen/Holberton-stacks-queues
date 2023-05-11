024ec178eeea8510cda35541b49bb4ff6c1b1003
nachofen <zorba037@gmail.com>
2023-05-11 12:03:21 -0700

    fix swap fprintf

--- functions2.c
+++ functions2.c
@@ -112,7 +112,7 @@ void monty_swap(stack_t **stack, unsigned int line_number)
 
 	if ((*stack) == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stdout, "L%d: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	aux = (*stack)->n;
